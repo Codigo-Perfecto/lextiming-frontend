16eac0102f2a5899c570b7454b491108ef7b41a4
Nicolas <germanrom92@gmail.com>
2026-09-02 20:23:10 -0300

    Cambio de ruta en cliente.service

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -8,7 +8,10 @@ import { Cliente } from '../models/cliente.model';
   providedIn: 'root'
 })
 export class ClienteService {
-  private apiUrl = environment.apiUrl;
+  //  Detección automática del entorno
+  private apiUrl = window.location.hostname === 'localhost' 
+    ? environment.apiUrl 
+    : 'https://lextiming-backend.onrender.com/api';     // URL de desarrollo
 
   constructor(private http: HttpClient) {}
 
